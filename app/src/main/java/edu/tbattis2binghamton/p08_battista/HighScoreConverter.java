7f4a88816c0d50b5055703177841e5d20dc5a54f
thomasbattista <thomasb0027@live.com>
2017-06-09 02:03:47 -0400

    Initial commit to github

---
+++ app/src/main/java/edu/tbattis2binghamton/p08_battista/HighScoreConverter.java
@@ -0,0 +1,8 @@
+package edu.tbattis2binghamton.p08_battista;
+
+/**
+ * Created by thomas on 5/25/17.
+ */
+
+public class HighScoreConverter {
+}
---
+++ app/src/main/java/edu/tbattis2binghamton/p08_battista/ScoreListAdapter.java
@@ -0,0 +1,8 @@
+package edu.tbattis2binghamton.p08_battista;
+
+/**
+ * Created by thomas on 6/8/17.
+ */
+
+public class ScoreListAdapter {
+}
